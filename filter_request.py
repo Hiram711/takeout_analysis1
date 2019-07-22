a5fd007d23c44786a91dda91d66a2929ae4a0338
hiram47 <371721772@qq.com>
2019-07-22 22:02:15 +0800

    按距离排序后的数据才会被抓取

--- filter_request.py
+++ filter_request.py
@@ -2,6 +2,8 @@ from mitmproxy import ctx
 import json
 from bs4 import BeautifulSoup
 
+from mitmproxy.http import HTTPFlow
+
 
 def response(flow):
     url1 = 'https://h5.waimai.meituan.com/waimai/mindex/home'
@@ -13,6 +15,7 @@ def response(flow):
             f.write(soup.prettify())
 
     if flow.request.url.startswith(url2):
-        text = flow.response.text
-        data = json.loads(text)
-        info(str(data))
+        if flow.request.urlencoded_form.get('sortId') == '5':
+            text = flow.response.text
+            data = json.loads(text)
+            info(str(data))
